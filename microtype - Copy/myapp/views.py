8a31cd63ed8efb23503c1de263b00d618c678163
Prakhar Garg <142152761+Prakhar-Garg7@users.noreply.github.com>
2024-03-15 17:15:30 +0530

    Add files via upload

---
+++ microtype - Copy/myapp/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from .models import History
+
+# def index(request):
+#     # last_login_user = User.objects.last()  # Get the last logged-in user
+#     # return render(request, 'index.html', {'last_login_user': last_login_user})
+#     return render(request,'index.html')
+
+
+
+def signup(request):
+    if(request.method=="POST"):
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        email=request.POST['email']
+        username=request.POST['username']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+
+        data=User.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password1)
+        data.save()
+        return redirect('login')
+    
+    return render(request,'register.html')
+
+def Login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        global curr_user
+        curr_user=username
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+        else:
+            return render(request,'login.html')
+        
+        if not History.objects.filter(user=user).exists():
+            History.objects.create(user=user)
+        return redirect('landing')
+            
+
+    return render(request,'login.html')
+
+# def Home(request):
+#     return render(request,'index.html')
+
+# def Home(request):
+#     last_login_user = User.objects.last()  # Get the last logged-in user
+#     return render(request, 'index.html', {'last_login_user': last_login_user})
+#     # return render(request,'index.html')
+def Home(request):
+    text={'username':curr_user}
+
+    return render(request,'index.html',text)
+
+def Landing(request):
+    text={'username':curr_user}
+
+    return render(request,'landing.html',text)
+
+def Guidance(request):
+    text={'username':curr_user}
+
+    return render(request,'guidance.html',text)
