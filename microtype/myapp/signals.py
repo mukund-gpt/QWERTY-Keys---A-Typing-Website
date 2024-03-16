1a9eba55cfe2a9f174f691d1b375e0783c849654
Mukund Bihari Gupta <142800930+mukund-gpt@users.noreply.github.com>
2024-03-16 18:05:18 +0530

    Delete microtype directory

--- microtype/myapp/signals.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-
-# Create a signal receiver function
-@receiver(user_logged_in)
-def print_last_login_user(sender, request, user, **kwargs):
-    # Print the username of the last login user
-    print("Last login user=>", user.username)
